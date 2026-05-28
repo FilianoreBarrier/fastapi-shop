b0dc9605a2e3c7c0fef0d83cb345e6bf108cb812
FilianoreBarrier <maric.zubenko@gmail.com>
2026-05-28 16:32:09 +0500

    add frontend(vue)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+// frontend/vite.config.js
+/**
+ * Конфигурация Vite для Vue проекта.
+ * Настройка dev сервера и алиасов путей.
+ */
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: '0.0.0.0',  // ← вот это
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
     },
   },
 })
